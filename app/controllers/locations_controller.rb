120570f0067293bb36c277ce2ed7ff21b9b2e4b3
Christophe Leray <christophe.leray1@gmail.com>
2020-10-21 15:45:43 +0200

    revert controller def create

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -21,7 +21,7 @@ class LocationsController < ApplicationController
     
     def create
         @location = Location.new(locations_params)
-        Rails.env.development? ? @location.user_id = current_user.id : @location.user = current_user
+        @location.user_id = current_user.id
         if @location.save
             redirect_to @location, notice: 'Location was succcessfully created'
         else
